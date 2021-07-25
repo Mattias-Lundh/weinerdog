a660854b0d0962121a8961d41d4b39ac02f2e5e0
mattias lundh <mattiasanderslundh@gmail.com>
2021-07-25 09:52:25 +0200

    shuffle

---
+++ weinerdog/weinerdog/strings/StringExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace weinerdog.strings
+{
+    public static class StringExtensions
+    {
+        public static string Suffle(this string s)
+        {
+            string output = "";
+            int arraysize = s.Length;
+            int[] randomArray = new int[arraysize];
+
+            for (int i = 0; i < arraysize; i++)
+            {
+                randomArray[i] = i;
+            }
+
+            Fisher_Yates(randomArray);
+
+            for (int i = 0; i < arraysize; i++)
+            {
+                output += s[randomArray[i]];
+            }
+
+            return output;
+        }
+
+        static System.Random rnd = new System.Random();
+
+        private void Fisher_Yates(int[] array)
+        {
+            int arraysize = array.Length;
+            int random;
+            int temp;
+
+            for (int i = 0; i < arraysize; i++)
+            {
+                random = i + (int)(rnd.NextDouble() * (arraysize - i));
+
+                temp = array[random];
+                array[random] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
